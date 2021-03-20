85a7bf95ebe10c340a7d83e88ccd590bbab2f54f
Alan Pinho <alanqupinho@gmail.com>
2021-03-20 20:18:15 -0300

    feat: :sparkles: New attributes: Automaton

--- src/Automaton.py
+++ src/Automaton.py
@@ -1,6 +1,10 @@
 from Transition import Transition
 
 class Automaton:
-    def __init__(self, name:str, transitions:Transition):
+    def __init__(self, name:str, states:list, language:list, transitions:list, initials:list, finals:list):
         self.name = name
+        self.states = states
+        self.language = language
         self.transitions = transitions
+        self.initials = initials
+        self.finals = finals
