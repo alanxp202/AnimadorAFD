b305874e4556bbca2b6159662d0e7ac38ea35d28
Alan Pinho <alanqupinho@gmail.com>
2021-03-20 15:31:02 -0300

    refactor: :recycle: remove old code

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-import pydot as pydot
-import os
-
-#os.environ["PATH"] += os.pathsep + 'C:/Users/alanq/scoop/apps/graphviz/2.44.1/bin'
-
+import pydot
 
 graphs = pydot.graph_from_dot_file('example.dot')
 graph = graphs[0]
